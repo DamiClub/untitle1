9c36c54f37e79d9b68ed878d23d85391b6ee779a
jiangdami <1046403080@qq.com>
2022-03-25 14:35:08 +0800

    modify main window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->setText("Hello! This is my first Git progame!");
 }
 
 MainWindow::~MainWindow()
