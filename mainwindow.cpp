74e57c24bea98be6c186053ed95a90d3f2471306
jiangdami <1046403080@qq.com>
2022-03-25 14:36:23 +0800

    change "progame" -> "program"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->label->setText("Hello! This is my first Git progame!");
+    ui->label->setText("Hello! This is my first Git program!");
 }
 
 MainWindow::~MainWindow()
